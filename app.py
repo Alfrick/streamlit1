2b89e65d6957ca57ade098d44406072bc8453ebc
Alfrick <alfo.opidi85@gmail.com>
2023-05-24 07:47:25 +0300

    made changes

--- app.py
+++ app.py
@@ -1,38 +1,71 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import inspect
+import textwrap
+
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from demo_echarts import ST_DEMOS
+from demo_pyecharts import ST_PY_DEMOS
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def main():
+    st.title("Streamlit ECharts Demo")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    with st.sidebar:
+        st.header("Configuration")
+        api_options = ("echarts", "pyecharts")
+        selected_api = st.selectbox(
+            label="Choose your preferred API:",
+            options=api_options,
+        )
 
-    points_per_turn = total_points / num_turns
+        page_options = (
+            list(ST_PY_DEMOS.keys())
+            if selected_api == "pyecharts"
+            else list(ST_DEMOS.keys())
+        )
+        selected_page = st.selectbox(
+            label="Choose an example",
+            options=page_options,
+        )
+        demo, url = (
+            ST_DEMOS[selected_page]
+            if selected_api == "echarts"
+            else ST_PY_DEMOS[selected_page]
+        )
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        if selected_api == "echarts":
+            st.caption(
+                """ECharts demos are extracted from https://echarts.apache.org/examples/en/index.html, 
+            by copying/formattting the 'option' json object into st_echarts.
+            Definitely check the echarts example page, convert the JSON specs to Python Dicts and you should get a nice viz."""
+            )
+        if selected_api == "pyecharts":
+            st.caption(
+                """Pyecharts demos are extracted from https://github.com/pyecharts/pyecharts-gallery,
+            by copying the pyecharts object into st_pyecharts. 
+            Pyecharts is still using ECharts 4 underneath, which is why the theming between st_echarts and st_pyecharts is different."""
+            )
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    demo()
+
+    sourcelines, _ = inspect.getsourcelines(demo)
+    with st.expander("Source Code"):
+        st.code(textwrap.dedent("".join(sourcelines[1:])))
+    st.markdown(f"Credit: {url}")
+
+
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="Streamlit ECharts Demo", page_icon=":chart_with_upwards_trend:"
+    )
+    main()
+    with st.sidebar:
+        st.markdown("---")
+        st.markdown(
+            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
+            unsafe_allow_html=True,
+        )
